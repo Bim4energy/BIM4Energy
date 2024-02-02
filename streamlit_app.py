60cafb4311042f5b40cbc3838ce1e0c1910c64cf
Bim4energy <158438523+Bim4energy@users.noreply.github.com>
2024-02-02 19:33:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ from stpyvista.utils import start_xvfb
 if "IS_XVFB_RUNNING" not in st.session_state:
     start_xvfb()
     st.session_state.IS_XVFB_RUNNING = True
+    
+st.set_page_config(layout="wide")
 
 # Define the buildingStandard dictionary
 buildingStandard = {
